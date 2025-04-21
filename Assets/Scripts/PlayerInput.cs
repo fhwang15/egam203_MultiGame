758de35ff463555850ea53d1f30bd99340d14994
fhwang15 <fhwang@inside.artcenter.edu>
2025-04-21 17:03:57 +0900

    This and that...
    - dead inside

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,16 +1,71 @@
+using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+    public float Score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI comboText;
+
+    private Dictionary<KeyCode, float> keyTimes = new Dictionary<KeyCode, float>();
+
+    public float inputWindow = 0.5f; // Time threshhold to check for key presses
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        CheckKey(KeyCode.W);
+        CheckKey(KeyCode.A);
+        CheckKey(KeyCode.S);
+        CheckKey(KeyCode.D);
+
+        if (IsComboMatched("W", "D"))
+        {
+            Debug.Log("Combo W + D matched!");
+        }
+        else if (IsComboMatched("A", "S"))
+        {
+            Debug.Log("Combo D + F matched!");
+        } else if (IsComboMatched("W", "S"))
+        {
+            Debug.Log("Combo W + S matched!");
+        }
+        else if (IsComboMatched("A", "D"))
+        {
+            Debug.Log("Combo A + D matched!");
+        }
+
+        scoreText.text = "Score: " + Score.ToString();
+
     }
+
+    void CheckKey(KeyCode key)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            keyTimes[key] = Time.time;
+        }
+    }
+
+    private bool IsComboMatched(string Key1, string Key2)
+    {
+        if (!keyTimes.ContainsKey(ToKeyCode(Key1)) || !keyTimes.ContainsKey(ToKeyCode(Key2))
+        {
+            return false;
+        }
+
+        float t1 = keyTimes[ToKeyCode(Key1)];
+        float t2 = keyTimes[ToKeyCode(Key2)];
+
+        return Mathf.Abs(t1 - t2) <= inputWindow;
+    }
+
+    KeyCode ToKeyCode(string s)
+    {
+        return (KeyCode)System.Enum.Parse(typeof(KeyCode), s.ToUpper());
+    }
+
 }
--- Assets/Scripts/RandomCodeGenerator.cs
+++ Assets/Scripts/RandomCodeGenerator.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
-
+using System.Collections.Generic;
+using TMPro;
 public class RandomCodeGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public List<string> validCombos = new List<string> { "WS", "WD", "AS", "AD" };
 
-    // Update is called once per frame
-    void Update()
+    public List<char> codeLetters = new List<char>();
+
+    //Generating a code that does not care about the order of the letters.
+    //The code should be 2 characters long.
+
+    public string Generate()
     {
-        
+        string combo = validCombos[Random.Range(0, validCombos.Count)];
+
+        // 정렬해서 순서 통일 (ex: "SW" -> "WS")
+        char[] letters = combo.ToCharArray();
+        System.Array.Sort(letters);
+        return new string(letters);
     }
 }
